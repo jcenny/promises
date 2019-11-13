434f8c117e27ca2c521417faef142cef7598eb73
jcenny <jcenny26@gmail.com>
2019-11-12 21:02:04 -0800

    Progress on advanced content collection.js

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,10 +7,29 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var readLine = require('./../bare_minimum/promiseConstructor.js');
+var Promise = require('bluebird');
+var fs = require('fs');
 
+var combineFirstLineOfManyFiles = function (filePaths, writePath) {
 
-var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  var promiseArr = [];
+
+  filePaths.forEach((element) => {
+    promiseArr.push(readLine.pluckFirstLineFromFileAsync(element));
+  });
+
+  return Promise.all(promiseArr).then(function (promiseResults) {
+    new Promise(function (resolve, reject) {
+      fs.writeFile(writePath, promiseResults.join('\n'), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  });
 };
 
 // Export these functions so we can unit test them
